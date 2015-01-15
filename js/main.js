bfe6571e10ef7f371304e6c90c5ef53124d9180c
Giovanni <giov@nnisouza.com>
2015-01-15 02:30:09 -0200

    parte superior e textos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
     var wrapper = $('.wrapper'),
+        upper = $('.upper'),
+        leftFX = $('.upper .leftFX'),
+        leftFX2 = $('.upper .leftFX2'),
+        grayTape = $('.grayTape'),
         primalForm = $('#primalForm'),
         progress = $('.progress'),
         subscribe = $('.grayTape button#subscribe'),
         thanks = $('.thanks'),
         w = wrapper.width(),
-        h = wrapper.height();
+        h = wrapper.height(),
+        gh = grayTape.height();
+        uh = h - gh;
+    
+    upper.height(uh);
     
     wrapper.fadeIn('medium');
+    leftFX.removeClass('hidden');
+    leftFX.addClass('fadeInUp animated');
+    
     subscribe.click(secondStep);
     
     
@@ -16,14 +27,22 @@ $(document).ready(function() {
         wrapper.addClass('secondStep');
         
         primalForm.addClass('fadeOutUp animated');
+        leftFX.addClass('fadeOutDown');
+        
+        leftFX2.removeClass('hidden');
+        leftFX2.addClass('fadeInLeft animated');
         
         progress.removeClass('hidden');
         progress.addClass('fadeInUp animated');
+        
         
         thanks.addClass('show');
         setTimeout(function() {
             thanks.removeClass('show');
         }, 4000);
+        setTimeout(function() {
+            upper.height(uh);
+        }, 600);
         
         return false;
     }
